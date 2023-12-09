08c0d68ce0973aaa14ea205134416d69f9d01694
starr <flourishamao@gmail.com>
2023-12-09 19:16:05 +0100

    work

--- nana.js
+++ nana.js
@@ -1,13 +1,13 @@
 function calc() {
     let expense = Number(phone.value) + Number(food.value) + Number(coffee.value) + Number(health.value) + Number(trans.value) + Number(oth.value)
     if (expense > 10000) {
-      err.innerHTML = "You can't spend more than #10,000 in a day!"
+      disp.innerHTML = "You can't spend above $10000 in a day!"
     }else if (phone.value == '' && food.value == '' && coffee.value == '' && health.value == '' && trans.value == '' && oth.value == '') {
-      err.innerHTML = "Fill in all fields!"
+   
     }
        
     else {
-      exp.innerHTML = "-#" + expense
+      exp.innerHTML = "-$" + expense
       bal.innerHTML = "$" + (300000 - expense)
       daily.innerHTML = "$" + (10000 - expense)
     }
